361c62d478d01609312871f12b9193fd40affcd3
Nikhil <vallalanikhil@gmail.com>
2020-08-06 23:54:32 +0530

    done

--- src/model/Blog.java
+++ src/model/Blog.java
@@ -1,2 +1,34 @@
 package model;
-// Type your code
+import java.time.LocalDate;
+
+class Blog{
+	String title;
+	int description;
+	LocalDate postedOn;
+ 
+	Blog(String title,int description,LocalDate postedOn){
+		this.title=title;
+		this.description=description;
+		this.postedOn=postedOn;
+		
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getDescription() {
+		return description;
+	}
+	public void setDescription(int description) {
+		this.description = description;
+	}
+	public LocalDate getPostedOn() {
+		return postedOn;
+	}
+	public void setPostedOn(LocalDate postedOn) {
+		this.postedOn = postedOn;
+	}
+	
+}
